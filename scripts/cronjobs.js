f95ac71016ef498a1f579da17322fe089d7de981
Pedro Santiago <phsantiago32@gmail.com>
2018-02-23 14:34:42 -0300

    add btc probe to notify every 45 min

--- scripts/cronjobs.js
+++ scripts/cronjobs.js
@@ -24,7 +24,7 @@ function cron(robot) {
 
     new HubotCron(cronPattern, timezone, breakTime);
 
-    var btcMonitor = function () {
+    var btcAlertProbe = function () {
         var requestOptions = {
             uri: 'https://api.bitcointrade.com.br/v1/market/estimated_price?amount=1&currency=BTC&type=buy',
             method: 'GET',
@@ -42,19 +42,17 @@ function cron(robot) {
 
                 var dateNow = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
 
-                if (price <= 32000 && shouldAlert(lastAlertDate, dateNow)) {
+                if (price <= 32000) {
                     lastAlertDate = dateNow;
-                    bot.sendMessage("65171887", "ALERTA DE BAIXA! Preço de venda do BTC abaixo de 32k: R$" + price + "(" + dateNow + ")");
+                    bot.sendMessage("65171887", "ALERTA DE BAIXA! Preço de venda do BTC abaixo de 32k: R$" + price + " (" + dateNow + ")");
                 }
                 else if (price >= 39000) {
                     lastAlertDate = dateNow;
-                    bot.sendMessage("65171887", "ALERTA DE ALTA! Preço de venda do BTC acima de 39k:" + price + "(" + dateNow + ")");
+                    bot.sendMessage("65171887", "ALERTA DE ALTA! Preço de venda do BTC acima de 39k: R$" + price + " (" + dateNow + ")");
                 }
-                else {
-                    bot.sendMessage("65171887","Preço de venda do BTC:" + price + "(" + dateNow + ")");
-                }
-            } else {
-                bot.sendMessage("65171887", "Erro ao sondar BTC");
+            }
+            else {
+                bot.sendMessage("65171887", "Erro ao sondar BTC (alerta)");
             }
         });
 
@@ -67,7 +65,35 @@ function cron(robot) {
         }
     }
 
-    new HubotCron('*/5 * * * *', timezone, btcMonitor);
+    new HubotCron('*/5 * * * *', timezone, btcAlertProbe);
+
+
+    var btcProbe = function () {
+        var requestOptions = {
+            uri: 'https://api.bitcointrade.com.br/v1/market/estimated_price?amount=1&currency=BTC&type=buy',
+            method: 'GET',
+            headers: {
+                'Authorization': 'ApiToken ' + btcToken
+            }
+        };
+
+        request(requestOptions, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var r = JSON.parse(body);
+
+                var price = parseFloat(Math.round(r.data.price * 100) / 100).toFixed(2);
+
+                var dateNow = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+
+                bot.sendMessage("65171887", "Preço de venda do BTCR$" + price + " (" + dateNow + ")");
+            }
+            else {
+                bot.sendMessage("65171887", "Erro ao sondar BTC");
+            }
+        });
+    }
+
+    new HubotCron('*/45 * * * *', timezone, btcProbe);
 }
 
 module.exports = cron;
